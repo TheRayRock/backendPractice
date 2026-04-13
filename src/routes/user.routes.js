6c2813a8b419b06308c0e979cb3c875960e5d53f
shahanwaz <mdshahnawazz0806@gmail.com>
2026-04-13 17:51:25 +0530

    add user routes for account management and booking history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,12 @@ import {
   registerUser,
   refreshAccessToken,
   getCurrentuser,
+  changeCurrentPassword,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getuserChanelProfil,
+  getWatchHistory,
 } from "../controller/user.controller.js";
 import multer from "multer";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -33,6 +39,14 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/customerList").get(verifyJWT, getCurrentuser);
+router.route("/current-user").get(verifyJWT, getCurrentuser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("coverImg")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getuserChanelProfil);
+router.route("history").get(verifyJWT, getWatchHistory);
 
 export default router;
